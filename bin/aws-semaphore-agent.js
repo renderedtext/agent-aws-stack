16adb97a518fe28e0c21c1a98334dca8e51f7ed1
Lucas Pinheiro <lpinheiro@renderedtext.com>
2021-12-06 16:19:53 -0300

    Use environment variables instead of cfn parameters

--- bin/aws-semaphore-agent.js
+++ bin/aws-semaphore-agent.js
@@ -2,20 +2,26 @@
 
 const cdk = require('@aws-cdk/core');
 const { AwsSemaphoreAgentStack } = require('../lib/aws-semaphore-agent-stack');
+const { ArgumentStore } = require('../lib/argument-store');
 
 const app = new cdk.App();
+
 new AwsSemaphoreAgentStack(app, 'AwsSemaphoreAgentStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  argumentStore: buildArgumentStore(),
+  description: "Semaphore agent autoscaling stack",
+  stackName: "aws-semaphore-agent-stack",
+  tags: {},
+  env: {
+    account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION
+  },
 });
+
+function buildArgumentStore() {
+  try {
+    return ArgumentStore.fromEnv();
+  } catch (e) {
+    console.error("ERROR: could not retrieve all required arguments from environment:", e)
+    process.exit(1);
+  }
+}
