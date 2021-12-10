b4aa20e3a0bab1115cd49ff49c3bfd439dcc4c65
Lucas Pinheiro <lpinheiro@renderedtext.com>
2021-12-10 17:08:58 -0300

    Describe asg + little refactor

--- lambdas/agent-scaler/app.js
+++ lambdas/agent-scaler/app.js
@@ -34,6 +34,35 @@ function getSemaphoreApiToken(apiTokenParameterName) {
   });
 }
 
+function describeAsg(asgName) {
+  var autoscaling = new aws.AutoScaling();
+
+  return new Promise(function(resolve, reject) {
+    var params = {
+      AutoScalingGroupNames: [asgName]
+    };
+
+    autoscaling.describeAutoScalingGroups(params, function(err, data) {
+      if (err) {
+        console.log("Error describing asg: ", err);
+        reject(err);
+      } else {
+        let autoScalingGroups = data.AutoScalingGroups;
+        if (autoScalingGroups.length === 0) {
+          reject(`Could not find auto scaling group '${asgName}'`);
+        } else {
+          let asg = autoScalingGroups[0];
+          resolve({
+            name: asg.AutoScalingGroupName,
+            desiredCapacity: asg.DesiredCapacity,
+            maxSize: asg.MaxSize
+          });
+        }
+      }
+    });
+  });
+}
+
 function executeRequest(token, nextPageToken) {
   const url = "/api/v1alpha/jobs?states=QUEUED&states=RUNNING";
   const options = {
@@ -88,6 +117,22 @@ function countByState(jobs, machineType) {
     }, initialMap);
 }
 
+function scaleUpIfNeeded(metrics, asg) {
+  const totalJobs = Object.keys(metrics).reduce((count, state) => count + metrics[state], 0);
+
+  console.log(`Job metrics: `, metrics);
+  console.log(`Current asg state: `, asg);
+  console.log(`Total jobs: ${totalJobs}, current capacity: ${asg.desiredCapacity}`);
+
+  if (totalJobs > asg.desiredCapacity) {
+    const desired = totalJobs > asg.maxSize ? asg.maxSize : totalJobs;
+    console.log(`Scaling up '${asg.name}' to ${desired} agents...`);
+    // TODO: actually scale asg
+  } else {
+    console.log(`No need to scale up.`);
+  }
+}
+
 exports.handler = async (event, context, callback) => {
   const apiTokenParameterName = process.env.SEMAPHORE_API_TOKEN_PARAMETER_NAME;
   if (!apiTokenParameterName) {
@@ -108,10 +153,22 @@ exports.handler = async (event, context, callback) => {
     };
   }
 
+  const asgName = process.env.SEMAPHORE_AGENT_ASG_NAME;
+  if (!asgName) {
+    console.error("No SEMAPHORE_AGENT_ASG_NAME specified.")
+    return {
+      statusCode: 500,
+      message: "error",
+    };
+  }
+
   try {
     const semaphoreApiToken = await getSemaphoreApiToken(apiTokenParameterName);
     const metrics = await getJobMetrics(semaphoreApiToken, agentType);
-    console.log(`Successfully fetched metrics for '${agentType}': `, metrics);
+    const asg = await describeAsg(asgName);
+
+    scaleUpIfNeeded(metrics, asg);
+
     return {
       statusCode: 200,
       message: "success",
