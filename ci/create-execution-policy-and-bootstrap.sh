2de657e3e8449473408717aa0ce7d158af709433
Lucas Pinheiro <lpinheiro@renderedtext.com>
2022-03-22 12:51:29 -0300

    Fix execution policy creation

--- ci/create-execution-policy-and-bootstrap.sh
+++ ci/create-execution-policy-and-bootstrap.sh
@@ -22,25 +22,50 @@ policy_arn="arn:aws:iam::${aws_account_id}:policy/${policy_name}"
 
 policy=$(aws iam get-policy --policy-arn "${policy_arn}" --query 'Policy.Arn' --output text)
 if [[ $? -eq 0 ]]; then
-  echo "Deleting ${policy_arn}..."
-  aws iam delete-policy --policy-arn ${policy_arn}
-  echo "${policy_arn} deleted."
-fi
+  echo "Deleting old versions of ${policy_arn}..."
+  version_ids=($(aws iam list-policy-versions \
+    --policy-arn arn:aws:iam::762007957083:policy/agent-aws-stack-tester-permissions \
+    | jq -r '.Versions[] | select(.IsDefaultVersion | not) | .VersionId'))
 
-echo "Creating ${policy_arn}..."
-policy_arn=$(aws iam create-policy \
-  --policy-name ${policy_name} \
-  --policy-document file://$(pwd)/execution-policy.json \
-  --description "Policy used by Cloudformation to deploy the agent-aws-stack CDK application" \
-  --query 'Policy.Arn' \
-  --output text)
+  for version_id in "${version_ids[@]}"; do
+    echo "Deleting version ${version_id}..."
+    aws iam delete-policy-version \
+      --policy-arn ${policy_arn} \
+      --version-id ${version_id}
+  done
 
-if [[ $? -eq 0 ]]; then
-  echo "Policy '$policy_arn' created. Bootstrapping application..."
-  npm run bootstrap -- aws://${aws_account_id}/${aws_region} \
-    --cloudformation-execution-policies "${policy_arn}" \
-    --verbose
+  echo "All non-default versions of ${policy_arn} were deleted. Creating new version..."
+  new_version=$(aws iam create-policy-version \
+    --policy-arn ${policy_arn} \
+    --policy-document file://$(pwd)/execution-policy.json \
+    --set-as-default \
+    --query 'PolicyVersion.VersionId' \
+    --output text)
+
+  if [[ $? -ne 0 ]]; then
+    echo "Error creating new policy version. Exiting..."
+    exit 1
+  fi
+
+  echo "Created new version ${new_version} of ${policy_arn}."
 else
-  echo "Error creating policy. Exiting..."
-  exit 1
+  echo "Creating ${policy_arn}..."
+  policy_arn=$(aws iam create-policy \
+    --policy-name ${policy_name} \
+    --policy-document file://$(pwd)/execution-policy.json \
+    --description "Policy used by Cloudformation to deploy the agent-aws-stack CDK application" \
+    --query 'Policy.Arn' \
+    --output text)
+
+  if [[ $? -ne 0 ]]; then
+    echo "Error creating new policy. Exiting..."
+    exit 1
+  fi
+
+  echo "Created ${policy_arn}."
 fi
+
+echo "Bootstrapping application..."
+npm run bootstrap -- aws://${aws_account_id}/${aws_region} \
+  --cloudformation-execution-policies "${policy_arn}" \
+  --verbose
