a54a79d8f8b5301feecbb9a492735257ef7dcc78
Lucas Pinheiro <lpinheiro@renderedtext.com>
2022-08-24 15:23:23 -0300

    fix: unset all AWS vars/files on shutdown hook script (#81)

--- packer/linux/ansible/roles/agent/files/terminate-instance.sh
+++ packer/linux/ansible/roles/agent/files/terminate-instance.sh
@@ -2,16 +2,22 @@ token=$(curl -X PUT -H "X-aws-ec2-metadata-token-ttl-seconds: 60" --fail --silen
 instance_id=$(curl -H "X-aws-ec2-metadata-token: $token" --fail --silent --show-error --location "http://169.254.169.254/latest/meta-data/instance-id")
 region=$(curl -H "X-aws-ec2-metadata-token: $token" --fail --silent --show-error --location "http://169.254.169.254/latest/meta-data/placement/region")
 
+# We unset all AWS related variables to make sure the instance profile is always used.
+# Before, we were using a specific AWS CLI profile that activates the instance profile,
+# but that didn't work if people messed up the ~/.aws/config file.
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
+rm -rf $HOME/.aws/credentials
+
 if [[ $SEMAPHORE_AGENT_SHUTDOWN_REASON == "IDLE" ]]; then
   aws autoscaling terminate-instance-in-auto-scaling-group \
     --region "$region" \
     --instance-id "$instance_id" \
-    --profile "semaphore__agent-aws-stack-instance-profile" \
     --should-decrement-desired-capacity
 else
   aws autoscaling terminate-instance-in-auto-scaling-group \
     --region "$region" \
     --instance-id "$instance_id" \
-    --profile "semaphore__agent-aws-stack-instance-profile" \
     --no-should-decrement-desired-capacity
 fi
