4cd15a2da47cef3b77b3c5a748ee983a9922edd6
Lucas Pinheiro <lpinheiro@renderedtext.com>
2023-07-10 10:30:05 -0300

    fix: use poweroff on health check script

--- packer/macos/files/health-check.sh
+++ packer/macos/files/health-check.sh
@@ -8,25 +8,15 @@ else
   export PATH=/usr/local/bin:$PATH
 fi
 
-mark_as_unhealthy() {
-  local __token__=$(curl -X PUT -H "X-aws-ec2-metadata-token-ttl-seconds: 60" --fail --silent --show-error --location "http://169.254.169.254/latest/api/token")
-  local __instance_id__=$(curl -H "X-aws-ec2-metadata-token: $__token__" --fail --silent --show-error --location "http://169.254.169.254/latest/meta-data/instance-id")
-
-  # We unset all AWS related variables to make sure the instance profile is used for this.
-  unset AWS_ACCESS_KEY_ID
-  unset AWS_SECRET_ACCESS_KEY
-  unset AWS_SESSION_TOKEN
-  rm -rf $HOME/.aws/credentials
-
-  aws autoscaling set-instance-health \
-    --instance-id "${__instance_id__}" \
-    --health-status Unhealthy
-}
-
 procs=$(ps aux | grep "/opt/semaphore/agent/agent start" | grep -v grep)
 if [[ -z ${procs} ]]; then
   echo "[$(date '+%Y-%m-%d %H:%M:%S')] Agent is not running - marking instance as unhealthy."
-  mark_as_unhealthy
+
+  # We shut down the instance using the OS here (instead of IMDS and autoscaling API),
+  # because this script will only be needed when the agent itself can't terminate the instance
+  # using IMDS and the autoscaling API. That can happen because the job messed up with the
+  # IMDS setup in the VM or some other weird behavior.
+  poweroff
 else
   echo "[$(date '+%Y-%m-%d %H:%M:%S')] Agent is running."
 fi
