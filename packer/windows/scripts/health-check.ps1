4307fc4b73f461843715b8474929a2494a733601
Lucas Pinheiro <lpinheiro@renderedtext.com>
2023-07-24 15:18:46 -0300

    fix health check logging

--- packer/windows/scripts/health-check.ps1
+++ packer/windows/scripts/health-check.ps1
@@ -1,12 +1,8 @@
 $proc = Get-Process | Where {$_.Path -Like "C:\semaphore-agent\agent.exe"}
 if ($proc) {
-  Add-Content `
-    -Path C:\semaphore-agent\health-check.log `
-    -Value "[$(Get-Date -Format 'dd/MM/yyyy HH:mm')] Agent is running."
+  "[$(Get-Date -Format 'dd/MM/yyyy HH:mm')] Agent is running." | Out-File -FilePath C:\semaphore-agent\health-check.log -Append -Encoding utf8
 } else {
-  Add-Content `
-    -Path C:\semaphore-agent\health-check.log `
-    -Value "[$(Get-Date -Format 'dd/MM/yyyy HH:mm')] Agent is not running - marking instance as unhealthy."
+  "[$(Get-Date -Format 'dd/MM/yyyy HH:mm')] Agent is not running - marking instance as unhealthy." | Out-File -FilePath C:\semaphore-agent\health-check.log -Append -Encoding utf8
 
   # We shut down the instance using the OS here (instead of IMDS and autoscaling API),
   # because this script will only be needed when the agent itself can't terminate the instance
