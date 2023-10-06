750e51def0667b5de8a6faa895aea96c2b6acf93
Lucas Pinheiro <lpinheiro@renderedtext.com>
2023-10-06 19:18:17 -0300

    wip: use pre-signed URL as registration name

---
+++ packer/linux/ansible/roles/agent/files/gen-pre-signed-url.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+import boto3
+client = boto3.client('sts')
+url = client.generate_presigned_url('get_caller_identity', Params={}, ExpiresIn=15*60, HttpMethod='GET')
+print(url)
