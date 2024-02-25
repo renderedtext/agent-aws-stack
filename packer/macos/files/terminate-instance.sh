f27c1a8dd42fa869c189899ee14cb396eed2a2e7
Luke Young <91491244+lyoung-confluent@users.noreply.github.com>
2024-02-25 12:40:50 -0800

    Update terminate-instance.sh

--- packer/macos/files/terminate-instance.sh
+++ packer/macos/files/terminate-instance.sh
@@ -11,7 +11,6 @@ fi
 
 token=$(curl -X PUT -H "X-aws-ec2-metadata-token-ttl-seconds: 60" --fail --silent --show-error --location "http://169.254.169.254/latest/api/token")
 instance_id=$(curl -H "X-aws-ec2-metadata-token: $token" --fail --silent --show-error --location "http://169.254.169.254/latest/meta-data/instance-id")
-ami_id=$(curl -H "X-aws-ec2-metadata-token: $token" --fail --silent --show-error --location "http://169.254.169.254/latest/meta-data/ami-id")
 region=$(curl -H "X-aws-ec2-metadata-token: $token" --fail --silent --show-error --location "http://169.254.169.254/latest/meta-data/placement/region")
 
 # We unset all AWS related variables to make sure the instance profile is always used.
@@ -28,6 +27,17 @@ if [[ $SEMAPHORE_AGENT_SHUTDOWN_REASON == "IDLE" ]]; then
     --instance-id "$instance_id" \
     --should-decrement-desired-capacity
 else
+  # Enter standby LifecycleState because the EC2 health check will fail while we're replacing the root volume
+  # We also decrement desired capacity so the ASG doesn't create a new replacement instance in the meantime
+  # The instance will exit standby automatically in start-agent.sh after reboot
+  asg_name=$(aws autoscaling describe-auto-scaling-instances --region "$region" --instance-ids "$instance_id" --output text --query "AutoScalingInstances[0].AutoScalingGroupName")
+  aws autoscaling enter-standby \
+    --region "$region" \
+    --instance-ids "$instance_id" \
+    --auto-scaling-group-name "$asg_name" \
+    --should-decrement-desired-capacity
+  # https://aws.amazon.com/blogs/compute/new-reset-amazon-ec2-mac-instances-to-a-known-state-using-replace-root-volume-capability/
+  ami_id=$(curl -H "X-aws-ec2-metadata-token: $token" --fail --silent --show-error --location "http://169.254.169.254/latest/meta-data/ami-id")
   aws ec2 create-replace-root-volume-task \
     --region "$region" \
     --instance-id "$instance_id" \
